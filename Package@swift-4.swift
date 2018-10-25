deb6102a4c8c14fd7cdd532504a31a7493488a32
andy <andrew.lees11@ibm.com>
2018-10-25 11:24:06 +0100

    Update to swift 4

---
+++ Package@swift-4.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SimpleLogger",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SimpleLogger",
+            targets: ["SimpleLogger"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SimpleLogger"
+        )
+    ]
+)
