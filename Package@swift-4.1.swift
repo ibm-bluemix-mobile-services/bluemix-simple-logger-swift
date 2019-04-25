c33a9faf4fd0074d4a2d502090a7dc1fb3fc876d
Ananth <agirijak@in.ibm.com>
2019-04-26 01:24:41 +0530

    updated package

--- Package@swift-4.1.swift
+++ Package@swift-4.1.swift
@@ -10,6 +10,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target defines a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "SimpleLogger"),
+        .target(name: "SimpleLogger",path: "Sources"),
     ]
 )
