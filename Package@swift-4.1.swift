4e3882aebabee30933a23944d48c084bb7c5e31d
Ananth <agirijak@in.ibm.com>
2019-04-26 01:28:51 +0530

    updated package for travis

--- Package@swift-4.1.swift
+++ Package@swift-4.1.swift
@@ -11,5 +11,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target defines a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "SimpleLogger",path: "Sources"),
+        .testTarget(
+            name: "SimpleLoggerTests",
+            dependencies: ["SimpleLogger"]),
     ]
 )
