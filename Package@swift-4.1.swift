4406918eebb2b8e7e19b3f9c29182627ed77eecd
Ananth <agirijak@in.ibm.com>
2019-04-26 01:18:52 +0530

    updated package

--- Package@swift-4.1.swift
+++ Package@swift-4.1.swift
@@ -2,5 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SimpleLogger"
+    name: "SimpleLogger",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(name: "SimpleLogger", targets: ["SimpleLogger"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "SimpleLogger"),
+    ]
 )
